e2ab27ecee79f1d388c86a7ad9d72e6eb06532cc
angel_ivanov <angelspsv@gmail.com>
2024-01-24 19:00:43 +0100

    Exercici 3

---
+++ exercici3.py
@@ -0,0 +1,7 @@
+#Exercici 3: demana 2 valors i dir quin és més gran
+entrada1 = input("Entra un numero:")
+entrada2 = input("Entra otro numero:")
+if entrada1 > entrada2:
+    print("Valor 1 es más grande que valor 2")
+else:
+    print("El valor 2 es más grande que el valor1")
