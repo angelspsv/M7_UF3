158c17ba7b9ee35191cbf524c8a621e7108a8919
angel_ivanov <angelspsv@gmail.com>
2024-01-24 22:44:06 +0100

    Exercici 4

---
+++ exercici4.py
@@ -0,0 +1,6 @@
+# Exercici 4: demanar 2 nombres a l'usuari i fer una operació matemàtica
+# mostrar el resultat
+num1 = int(input("Entra un primer número:"))
+num2 = int(input("Entra un segundo número:"))
+result = num1 * num2
+print("El resultado es:" , result)
