a41ce393e37690416357676486adc6f909dd4774
angel_ivanov <angelspsv@gmail.com>
2024-01-26 16:48:17 +0100

    Exercici 13

---
+++ exercici13.py
@@ -0,0 +1,6 @@
+# Exercici 13
+# demanar un número de l'1 al 10 i retornar la taula de 
+# multiplicació
+num = int(input("Entra un número de l'1 al 10: "))
+for i in range(10):
+    print((i+1)*num)
