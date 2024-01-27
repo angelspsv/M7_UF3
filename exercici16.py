7cea712f5a69ba871ea6711618dd4311d1a7781e
angel_ivanov <angelspsv@gmail.com>
2024-01-27 10:59:06 +0100

    Exercici 16

--- exercici16.py
+++ exercici16.py
@@ -3,5 +3,6 @@
 # el programa eliminarà els espais entre mots
 # l'afegirà a una tupla i el mostrarà
 text = input("Entra un text: ")
-lista = [letra for letra in text]
-print(lista)
+lista = [letra for letra in text if letra != ' ']
+mi_tupla = tuple(lista)
+print(mi_tupla)
