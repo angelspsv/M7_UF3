36621d2a7007b64871c8696810862d9102973209
angel_ivanov <angelspsv@gmail.com>
2024-02-01 14:45:28 +0100

    Exercici 28

---
+++ exercici28.py
@@ -0,0 +1,17 @@
+# Exercici 28: crear una funció
+# rebrà 2 números com paràmetres passats per l'usuari
+# retorna un número random entre aquests dos nombres
+import random
+
+def ObtenirDosNums():
+    num1 = int(input("Entra un número: "))
+    num2 = int(input("Entra un altre número: "))
+    RandomEntreDosNums(num1, num2)
+
+
+def RandomEntreDosNums(num1, num2):
+    # el nombre aleatori pot ser un dels dos números que serveixen com a límit
+    num_random = random.randint(num1, num2)
+    print(f'El número aleatori entre {num1} i {num2} és: {num_random}.')
+
+ObtenirDosNums()
