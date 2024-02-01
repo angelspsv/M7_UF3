b6d36a8e3e519df8ea56790616ec77165dacf8ca
angel_ivanov <angelspsv@gmail.com>
2024-02-01 23:25:45 +0100

    Exercici 29

---
+++ exercici29.py
@@ -0,0 +1,28 @@
+# Exercici 29
+# crear una funció que rep dos nombres per paràmetre i
+# ha de mostrar tots el nombres enters situats entre
+# ambdós. També ha de mostrar la suma de tots els nombres
+def NumerosEntreDosNums(num1, num2):
+    suma = 0
+    mi_rango = range(num1, num2)
+    for i in mi_rango:
+        print(i)
+        suma += i
+    # així la seqüència inclurà el número d'inici i fi
+    print(num2)
+    print(f'La suma total de tots el números entre els números és: {suma+num2}.')
+
+
+
+def DemanarDosNumeros():
+    num1 = int(input("Entra el primer número: "))
+    num2 = int(input("Entra el segon número: "))
+    if num1 < num2:
+        NumerosEntreDosNums(num1, num2)
+    elif num1 > num2:
+        NumerosEntreDosNums(num2, num1)
+    else:
+        print("Impossible, els dos números són el mateix")
+
+
+DemanarDosNumeros()
