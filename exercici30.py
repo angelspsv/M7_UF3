5b3dc1dc401d7ae3a358ee0f1421410161c6ef8f
angel_ivanov <angelspsv@gmail.com>
2024-02-01 14:46:11 +0100

    Exercici 30

---
+++ exercici30.py
@@ -0,0 +1,8 @@
+# Exercici 30: crear una funció que al rebre un nom
+# treu per sortida: Hola + nom_rebut 
+def demanaNom():
+    nom = input("Entra un nom: ")
+    print(f'Hola {nom}!')
+
+#crida a la funció
+demanaNom()
