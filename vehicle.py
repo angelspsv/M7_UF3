bc4a88bf3307a6cf940fbae43d84942191d310f4
angel_ivanov <angelspsv@gmail.com>
2024-02-02 16:06:55 +0100

    mètodes get i set fets

--- vehicle.py
+++ vehicle.py
@@ -1,6 +1,6 @@
 # Exercici 25: crear un fitxer vehicle.py 
 # definir la classe vehicle amb: constructor, 6 atributs,
-# Getters i Setters, mètode per mostrar totes les dades
+# Getters i Setters, mètode per mostrar totes les dades,
 # afegir el mètode to_dict(self) per retornar l'objecte
 # en format json
 class vehicle:
@@ -12,10 +12,51 @@ class vehicle:
         self.vel_max = vel_max
         self.combustible = combustible
     
+    # mètodes get i set per accedir als atributs
     def getMarca(self):
         return self.marca
     
     def setMarca(self, marca):
         self.marca = marca
     
+    def getModelo(self):
+        return self.modelo
     
+    def setModelo(self, modelo):
+        self.modelo = modelo
+    
+    def getAny(self):
+        return self.any
+    
+    def setAny(self, any):
+        self.any = any
+    
+    def getColor(self):
+        return self.color
+    
+    def setColor(self, color):
+        self.color = color
+    
+    def getVel(self):
+        return self.vel_max
+    
+    def setVel(self, vel_max):
+        self.vel_max = vel_max
+    
+    def getCombustible(self):
+        return self.combustible
+    
+    def setCombustible(self, combustible):
+        self.combustible = combustible
+    
+    # mètode que retorna un diccionari amb els valors de l'objecte
+    def parts(self):
+        return {
+            'marca': self.marca,
+            'modelo': self.modelo,
+            'any': self.any,
+            'color': self.color,
+            'vel_max': self.vel_max,
+            'combustible': self.combustible
+        }
+
