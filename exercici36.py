f93c44c1278a7b6d824e965be81a5f751d411f72
angel_ivanov <angelspsv@gmail.com>
2024-02-03 12:55:27 +0100

    Exercici 36

---
+++ exercici36.py
@@ -0,0 +1,13 @@
+# Exercici 36
+# cercar errors en el següent codi
+
+passwd = input("Introdueix una contrasenya: ")
+if passwd in ['Peliñ4nd0#']:
+    print("Correcte")
+else:
+    print("Incorrecte")
+
+# errors: nom de variable ha de ser el mateix
+# identació en el if/else
+# falten ':' a la condició else
+# parèntesi diferents al codi original '[' i ')'
