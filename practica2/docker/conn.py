28f0b1aa776bccb8884242db61a1b085c9eb7423
angel <angelspsv@gmail.com>
2024-02-07 18:55:21 +0100

    creat fitxer de conexio

---
+++ practica2/docker/conn.py
@@ -0,0 +1,17 @@
+# fitxer per la connexio a la bbdd
+from multiprocessing import connection
+import psycopg2
+
+# dades agafades des del fitxer .yml per la connexio a la bbdd
+conn = psycopg2.connect(
+    database='bbdd',
+    user='admin',
+    password='admin',
+    host='localhost',
+    port='5432'
+)
+
+# per fer la connexio s'utilitza el metode cursor()
+connection = conn.cursor()
+
+print(connection)
