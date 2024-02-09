39bc63bbbf8c3ca7da92d0fb24d905452850de07
angel <angelspsv@gmail.com>
2024-02-09 12:33:58 +0100

    funcio main i codi try-except

--- practica2/docker/Main.py
+++ practica2/docker/Main.py
@@ -1,9 +1,30 @@
 # fitxer Main.py
 # des d'aqui es gestionaran les peticions del client
+import psycopg2
+from psycopg2 import Error
+from conn import connection
+from create.py import create_mod
+from read.py import read_mod
+from update.py import update_mod
+from delete.py import delete_mod
 
-try:
 
+def main():
+    # obtenir la connexió des de conn.py
+    if not connection:
+        print("No es pot establir la connexió a la bbdd")
+        return
 
+    try:
+        # fer les crides de CRUD
+        data = "" #dades a inserir
+        create_mod(connection, data)
 
+    except Error as e:
+        print("Error: ", e)
+    finally:
+        if connection:
+            connection.close()
+            print("Connexió tancada")
 
-except:
+main()
