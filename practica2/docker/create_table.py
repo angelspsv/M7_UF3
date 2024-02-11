d65d7b7c3c3554b794d421f488b9b0457f31ebbf
angel <angelspsv@gmail.com>
2024-02-11 09:40:40 +0100

    petits canvis

--- practica2/docker/create_table.py
+++ practica2/docker/create_table.py
@@ -2,22 +2,23 @@
 import psycopg2
 from connection import *
 
-# per fer la connexio s'utilitza el metode cursor()
+def create_table_modul():
+    # per fer la connexio s'utilitza el metode cursor()
 
-sql = '''CREATE TABLE IF NOT EXISTS personas (
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(20),
-    apellido VARCHAR(30),
-    edad INTEGER,
-    email VARCHAR(50),
-    movil VARCHAR(15)
-)
-'''
+    sql = '''CREATE TABLE IF NOT EXISTS personas (
+        id SERIAL PRIMARY KEY,
+        nombre VARCHAR(20),
+        apellido VARCHAR(30),
+        edad INTEGER,
+        email VARCHAR(50),
+        movil VARCHAR(15)
+    )
+    '''
 
-# Amb el mètode execute() s'envia la query
-connection.execute(sql)
+    # Amb el mètode execute() s'envia la query
+    connection.execute(sql)
 
-# Commit per fer efectius els canvis de la query a la BD
-conn.commit()
+    # Commit per fer efectius els canvis de la query a la BD
+    conn.commit()
 
-print("Taula creada")
+    print("Taula creada")
