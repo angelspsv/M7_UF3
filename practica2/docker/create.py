057d12a9857894e1053a5f3912ff44c97f8d5c18
angel <angelspsv@gmail.com>
2024-02-11 09:43:00 +0100

    afegida aqui la entrada de dades per input per insert

--- practica2/docker/create.py
+++ practica2/docker/create.py
@@ -3,18 +3,27 @@ import psycopg2
 from connection import *
 
 def create_modul():
+
+    # demanar les dades de l'usuari des de la consola
+    print("Dades de l\'usuari per realitzar una nova entrada a la base de dades: ")
+    nom = input("Entra el teu nom: ")
+    cognom = input("Entra el teu cognom: ")
+    edat = int(input("Entra la teva edat: "))
+    email = input("Entra el teu correu: ")
+    movil = input("Entra el teu móvil: ")
         
     # Consulta SQL para insertar un nuevo registro en la tabla personas
     insert_query = '''
-        INSERT INTO public.personas (nombre, apellido, edad, email, movil) VALUES ('Daniel', 'Garcia', 22, 'dani@mail.com', '612345678')
+        INSERT INTO public.personas (nombre, apellido, edad, email, movil) VALUES (%s, %s, %s, %s, %s)
     '''
-        
+
+    # dades agafades per input per la query-insert
+    values = (nom, cognom, edat, email, movil)
+
     # Amb el mètode execute() s'envia la query
-    connection.execute(insert_query)
+    connection.execute(insert_query, values)
 
     # Commit per fer efectius els canvis de la query a la BD
     conn.commit()
         
     print("Registre creat correctament")
-
-create_modul()
