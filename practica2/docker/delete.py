3026284d60d294a640614f8e2cac07241d729f21
angel <angelspsv@gmail.com>
2024-02-14 17:48:12 +0100

    afegits comentaris

--- practica2/docker/delete.py
+++ practica2/docker/delete.py
@@ -3,16 +3,18 @@ import psycopg2
 from connection import *
 
 def delete_modul():
-    #consulta query per esbrinar quin és el darrer id
+    #consulta query per esbrinar quin és el darrer id disponible a la taula
     id_query = '''
         SELECT id FROM public.personas ORDER BY id DESC LIMIT 1
     '''
 
     # execució de la consulta SQL 
     connection.execute(id_query)
+
+    # resultat de la consulta sql
     last_id = connection.fetchone()[0]
 
-    # Consulta SQL per eliminar el últim registre a la taula
+    # Consulta SQL per eliminar el últim registre de la taula
     delete_query = '''
         DELETE FROM public.personas WHERE id = %s
     '''
@@ -22,7 +24,7 @@ def delete_modul():
     # execució de la consulta SQL per esborrar la darrera entrada de la taula
     connection.execute(delete_query, value)
 
-    # commit per desar els canvis a la base de dades
+    # commit per desar els canvis a la taula/base de dades
     conn.commit()
 
     print("Últim registre esborrat correctament.")
