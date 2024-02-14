b8893307f0f2e43280b3c7641ddbf58bb51096ef
angel <angelspsv@gmail.com>
2024-02-14 17:44:20 +0100

    afegit condicional IF al codi per executar el programa poc a poc

--- practica2/docker/Main.py
+++ practica2/docker/Main.py
@@ -21,15 +21,34 @@ def main():
         create_table.create_table_modul()
 
         # fer les crides de CRUD
-        create.create_modul()
-        read.read_modul()
+        entrada_insert = input("Vols fer una nova entrada de dades? (escriu 'si' en cas afirmatiu): ")
+        tmp = entrada_insert.lower()
+        if (tmp == 'si'):
+            # amb create_modul fem un insert a la taula
+            create.create_modul()
 
-        # demanar el nou número de móvil i executar la funció amb la dada
-        new_movil_value = input("Entra el nou número de móvil: ")
-        update.update_modul(new_movil_value)
         
-        # esborrar el darrer registre de la taula
-        delete.delete_modul()
+        entrada_read = input("Vols lleguir la darrera entrada feta a la taula? (escriu 'si' en cas afirmatiu): ")
+        tmp1 = entrada_read.lower()
+        if (tmp1 == 'si'):
+            # amb read_modul lleguim la darrera entrada de la taula personas
+            read.read_modul()
+
+
+        entrada_update = input("Vols actualitzar el valor de la columna 'movil' del darrer usuari? (escriu 'si' en cas afirmatiu): ")
+        tmp2 = entrada_update.lower()
+        if (tmp2 == 'si'):
+            # demanar el nou número de móvil i actualitzem el valor de la columna 'movil'
+            new_movil_value = input("Entra el nou número de móvil: ")
+            update.update_modul(new_movil_value)
+
+
+        entrada_delete = input("Vols esborrar la darrera entrada amb dades de la taula? (escriu 'si' en cas afirmatiu): ")
+        tmp3 = entrada_delete.lower()
+        if (tmp3 == 'si'):
+            # esborrar el darrer registre de la taula
+            delete.delete_modul()
+
 
     except Error as e:
         print("Error: ", e)
@@ -38,4 +57,5 @@ def main():
             connection.close()
             print("Connexió tancada")
 
+# crida del main.py per executar el programa sencer
 main()
